d0d29421e364b9ae04f9744e5dc105786bd38c18
Denise <denise.sollano@gmail.com>
2023-07-10 14:09:15 -0500

    Prettify index.html and main.js

--- main.js
+++ main.js
@@ -1,42 +1,36 @@
 //Navbar Functions
 // DROPDOWN NAV BAR CODE
 
-let rightBarBtn = document.getElementsByClassName('right-bar-btn')[0];
-let links = document.getElementsByClassName('links')[0];
-let backBtn = document.getElementsByClassName('back')[0];
+let rightBarBtn = document.getElementsByClassName("right-bar-btn")[0];
+let links = document.getElementsByClassName("links")[0];
+let backBtn = document.getElementsByClassName("back")[0];
 
-rightBarBtn.addEventListener('click', () => {
-    if (links.style.display != 'block') {
-        links.style.display = 'block';
+rightBarBtn.addEventListener("click", () => {
+    if (links.style.display != "block") {
+        links.style.display = "block";
         links.classList.add("appear");
         links.classList.remove("links-slide-up");
-    }
-    else {
+    } else {
         links.classList.add("appear");
         links.classList.remove("links-slide-up");
     }
 });
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     let screenwidth = window.innerWidth;
     if (screenwidth <= 700) {
-        links.style.display = 'none'
+        links.style.display = "none";
     }
     if (screenwidth > 700) {
-        links.style.display = 'flex';
+        links.style.display = "flex";
         links.classList.remove("links-slide-up");
-
-    };
+    }
 });
 
-backBtn.addEventListener('click', () => {
-  links.classList.remove("appear");
-  links.classList.add("links-slide-up");
+backBtn.addEventListener("click", () => {
+    links.classList.remove("appear");
+    links.classList.add("links-slide-up");
 });
-
-
-
-
 
 // APPEAR ON SCROLL COD
 const scrollElements = document.querySelectorAll(".js-scroll");
@@ -52,24 +46,27 @@ const elementInView = (el) => {
 // If the top of the element is more than the viewport height, then the element is not visible.
 const elementOutofView = (el) => {
     const elementTop = el.getBoundingClientRect().top;
-  
+
     return (
-      elementTop > (window.innerHeight || document.documentElement.clientHeight) // See if the top of the element is greater than the viewport height.
+        elementTop > (window.innerHeight || document.documentElement.clientHeight) // See if the top of the element is greater than the viewport height.
     );
-  };
+};
 
 const handleScrollAnimation = () => {
-    scrollElements.forEach((el) => { // For every element than has the .js-scroll class
-      if (elementInView(el)) { // If the top of the element is less than the viewport
-        el.classList.add("appear"); // Add the .appear class
-      } else if (elementOutofView(el)) { // If the top of the element is greater than the viewport
-        el.classList.remove("appear"); // Remove the .appear class
-      }
-    })
-  }
+    scrollElements.forEach((el) => {
+        // For every element than has the .js-scroll class
+        if (elementInView(el)) {
+            // If the top of the element is less than the viewport
+            el.classList.add("appear"); // Add the .appear class
+        } else if (elementOutofView(el)) {
+            // If the top of the element is greater than the viewport
+            el.classList.remove("appear"); // Remove the .appear class
+        }
+    });
+};
 
-window.addEventListener("scroll", () => { 
-handleScrollAnimation();
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
 });
 
 // FAILED VERSION OF ON SCROLL APPEARANCE
@@ -86,5 +83,5 @@ handleScrollAnimation();
 //         document.getElementsByClassName("text")[0].style.opacity = "0";
 //     }
 //     }
-    
+
 // }
